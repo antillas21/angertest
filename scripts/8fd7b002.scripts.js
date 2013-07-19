7fc5b7720847b7ab5ae4555dfb794723034753c1
Antonio Antillon <antillas21@gmail.com>
2013-07-18 23:23:01 -0700

    Initial commit

---
+++ scripts/8fd7b002.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("angtestApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("angtestApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma","Something else"]}]);
